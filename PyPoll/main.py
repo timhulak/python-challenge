3721ca0519cbed303c1d5f3ae72a3b2498921100
Tim <timhulak@gmail.com>
2018-12-16 08:47:55 -0800

    Finished with pyBank, working out some kinks in pyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,23 @@
 #import Dependencies/Modules
 import os
 import csv
-import sys
-import numpy as np
 
 #encoding path
 pyPoll_path = os.path.join("resources","pyPoll.csv")
 
-#Vars and Lists to store data
-votes = []
-
 with open(pyPoll_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    for row in csvreader:
+
     #The total number of votes cast
-        votes.append(int(row[0]))
-    total_votes = sum(votes)
+    total_votes = sum(tuple([1 for row in csvreader]))
 
     #A complete list of candidates who received votes
-    canidate_list_all = tuple([row[2] for row in csvreader])
-    canidate_list_unique = []
-    for i in canidate_list_all:
-        if i not in canidate_list_unique:
-            canidate_list_unique.append(i)
-
+    canidate_list = tuple([row[2] for row in csvreader])
+    unique_canidate_list = []
+    for name in canidate_list:
+        if name not in unique_canidate_list:
+            unique_canidate_list.append(name)
 
     #The total number of votes each candidate won
     khan = 0
